80700943280561989c90f426439745287abe2977
Ketan Sonar <ketan.sonar.798@gmail.com>
2024-11-18 18:13:01 +0530

    add webzterm config

---
+++ wezterm/wezterm.lua
@@ -0,0 +1,16 @@
+local wezterm = require("wezterm")
+
+local config = wezterm.config_builder()
+
+config = {
+    font = wezterm.font({
+        family = "DejaVuSansM Nerd Font Mono",
+        -- harfbuzz_features = { "calt=0" }, -- this disables ligatures
+    }),
+    font_size = 16,
+    color_scheme = "Nancy (terminal.sexy)",
+    enable_tab_bar = false,
+    window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+}
+
+return config
