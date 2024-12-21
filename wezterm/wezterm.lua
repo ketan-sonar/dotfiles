a747f183bab6891d80a3379ef384597a2fab7944
Ketan Sonar <ketan.sonar.798@gmail.com>
2024-12-21 20:07:16 +0530

    update wezterm.lua

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -3,15 +3,19 @@ local wezterm = require("wezterm")
 local config = wezterm.config_builder()
 
 config = {
-    font = wezterm.font({
-        family = "DejaVuSansM Nerd Font Mono",
-        -- harfbuzz_features = { "calt=0" }, -- this disables ligatures
-    }),
-    font_size = 16,
-    color_scheme = "Nancy (terminal.sexy)",
-    enable_tab_bar = false,
-    window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
-    native_macos_fullscreen_mode = true,
+  font = wezterm.font({
+    family = "DejaVuSansM Nerd Font Mono",
+    -- harfbuzz_features = { "calt=0" }, -- this disables ligatures
+  }),
+  font_size = 16,
+  -- color_scheme = "tokyonight_night",
+  color_scheme = "Nancy (terminal.sexy)",
+  enable_tab_bar = false,
+  window_padding = { left = 0, right = 0, top = 0, bottom = 0 },
+  native_macos_fullscreen_mode = true,
+  -- window_decorations = "RESIZE",
+  -- window_background_opacity = 0.9,
+  -- macos_window_background_blur = 10,
 }
 
 return config
