73d1b0077abfa5f9fe4b41b57ea4cc82171b9254
Ketan Sonar <ketan.sonar.798@gmail.com>
2026-01-05 21:31:45 +0530

    update minimal neovim config

--- neovim-minimal/init.lua
+++ neovim-minimal/init.lua
@@ -1,8 +1,6 @@
-vim.g.mapleader = " "
-
 vim.opt.termguicolors = true
-
-vim.opt.guicursor = ""
+vim.opt.cursorline = true
+-- vim.opt.guicursor = ""
 
 vim.opt.number = true
 vim.opt.relativenumber = true
@@ -41,130 +39,234 @@ vim.opt.wildignore:append("*/target/*")
 vim.opt.iskeyword:remove("_")
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = "solidity",
-    callback = function()
-        vim.cmd("iabbrev uint uint256")
-    end,
+	pattern = "solidity",
+	callback = function()
+		vim.cmd("iabbrev uint uint256")
+	end,
 })
 
 vim.api.nvim_create_autocmd("FileType", {
-    pattern = {
-        "javascript",
-        "javascriptreact",
-        "typescript",
-        "typescriptreact",
-        "dart",
-        "schema",
-        "prisma",
-        "css",
-        "html",
-    },
-    callback = function()
-        vim.opt_local.tabstop = 2
-        vim.opt_local.softtabstop = 2
-        vim.opt_local.shiftwidth = 2
-    end,
+	pattern = {
+		"javascript",
+		"javascriptreact",
+		"typescript",
+		"typescriptreact",
+		"dart",
+		"schema",
+		"prisma",
+		"css",
+		"html",
+	},
+	callback = function()
+		vim.opt_local.tabstop = 2
+		vim.opt_local.softtabstop = 2
+		vim.opt_local.shiftwidth = 2
+	end,
 })
-
-vim.keymap.set("n", "<leader>h", "<C-w><C-h>")
-vim.keymap.set("n", "<leader>l", "<C-w><C-l>")
-vim.keymap.set("n", "<leader>j", "<C-w><C-j>")
-vim.keymap.set("n", "<leader>k", "<C-w><C-k>")
-
--- quick fix list
-vim.keymap.set("n", "<C-j>", "<cmd>cnext<cr>", { desc = "cnext" })
-vim.keymap.set("n", "<C-k>", "<cmd>cprev<cr>", { desc = "cprev" })
 
 vim.api.nvim_create_autocmd("TextYankPost", {
-    desc = "Highlight when yanking (copying) text",
-    group = vim.api.nvim_create_augroup(
-        "kickstart-highlight-yank",
-        { clear = true }
-    ),
-    callback = function()
-        vim.highlight.on_yank()
-    end,
+	desc = "Highlight when yanking (copying) text",
+	group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+	callback = function()
+		vim.highlight.on_yank()
+	end,
 })
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"--branch=stable",
+		lazyrepo,
+		lazypath,
+	})
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+require("lazy").setup({
+	spec = {
+		{
+			"nvim-treesitter/nvim-treesitter",
+			branch = "master",
+			lazy = false,
+			build = ":TSUpdate",
+			config = function()
+				require("nvim-treesitter.configs").setup({
+					ensure_installed = {},
+					sync_install = false,
+					auto_install = true,
+					modules = {},
+					ignore_install = {},
+					highlight = { enable = true },
+					incremental_selection = {
+						enable = true,
+						keymaps = {
+							node_incremental = "v",
+							scope_incremental = "<C-s>",
+							node_decremental = "V",
+						},
+					},
+					indent = { enable = true },
+				})
+			end,
+		},
+		{
+			"catppuccin/nvim",
+			name = "catppuccin",
+			priority = 1000,
+			---@module "catppuccin"
+			---@type CatppuccinOptions
+			opts = {
+				flavour = "mocha",
+				no_italic = true,
+				no_bold = true,
+				color_overrides = { mocha = { base = "#101019" } },
+			},
+			config = function(_, opts)
+				require("catppuccin").setup(opts)
+				vim.cmd.colorscheme("catppuccin")
+			end,
+		},
+		{
+			"neovim/nvim-lspconfig",
+			dependencies = {
+				{
+					"folke/lazydev.nvim",
+					ft = "lua",
+					dependencies = {
+						{
+							"DrKJeff16/wezterm-types",
+							lazy = true,
+							version = false,
+						},
+					},
+					opts = {
+						library = {
+							{
+								path = "${3rd}/luv/library",
+								words = { "vim%.uv" },
+							},
+							{
+                                path = "wezterm-types",
+                                mods = { "wezterm" }
+                            },
+						},
+					},
+				},
+			},
+		},
+		{ "mason-org/mason.nvim", opts = {} },
+		{
+			"mason-org/mason-lspconfig.nvim",
+			opts = {},
+			dependencies = {
+				{ "mason-org/mason.nvim", opts = {} },
+				"neovim/nvim-lspconfig",
+			},
+		},
+		{
+			"rachartier/tiny-inline-diagnostic.nvim",
+			event = "VeryLazy",
+			priority = 1000,
+			opts = {},
+		},
+		{
+			"saghen/blink.cmp",
+			dependencies = { "rafamadriz/friendly-snippets" },
+			version = "1.*",
+			---@module "blink-cmp"
+			---@type blink.cmp.Config
+			opts = {
+				keymap = { preset = "super-tab" },
+				signature = { enabled = true },
+			},
+			opts_extend = { "sources.default" },
+		},
+		{
+			"stevearc/conform.nvim",
+			opts = {
+				formatters_by_ft = {
+					lua = { "stylua" },
+					python = {
+						"ruff_fix",
+						"ruff_format",
+						"ruff_organize_imports",
+					},
+					rust = { "rustfmt", lsp_format = "fallback" },
+					javascript = { "prettierd" },
+					javascriptreact = { "prettierd" },
+					typescript = { "prettierd" },
+					typescriptreact = { "prettierd" },
+					go = { "gofmt", "goimports", "golines" },
+				},
+			},
+			config = function(_, opts)
+				local conform = require("conform")
+				conform.setup(opts)
+				vim.keymap.set("n", "<leader>cf", conform.format)
+			end,
+		},
+		{ "windwp/nvim-autopairs", event = "InsertEnter", opts = {} },
+		{
+			"christoomey/vim-tmux-navigator",
+			cmd = {
+				"TmuxNavigateLeft",
+				"TmuxNavigateDown",
+				"TmuxNavigateUp",
+				"TmuxNavigateRight",
+				"TmuxNavigatePrevious",
+				"TmuxNavigatorProcessList",
+			},
+			keys = {
+				{ "<c-h>", "<cmd><C-U>TmuxNavigateLeft<cr>" },
+				{ "<c-j>", "<cmd><C-U>TmuxNavigateDown<cr>" },
+				{ "<c-k>", "<cmd><C-U>TmuxNavigateUp<cr>" },
+				{ "<c-l>", "<cmd><C-U>TmuxNavigateRight<cr>" },
+				{ "<c-\\>", "<cmd><C-U>TmuxNavigatePrevious<cr>" },
+			},
+		},
+		{
+			"ibhagwan/fzf-lua",
+			dependencies = { "nvim-tree/nvim-web-devicons" },
+			config = function()
+				vim.keymap.set("n", "<leader>sf", ":FzfLua files<CR>")
+				vim.keymap.set("n", "<leader>sg", ":FzfLua live_grep<CR>")
+			end,
+		},
+	},
+	install = { colorscheme = { "habamax" } },
+	-- checker = { enabled = true },
+})
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function()
+		vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
+		vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+	end,
+})
+
+vim.keymap.set("n", "<leader>m", ":make<CR>")
+vim.keymap.set("n", "<leader>j", ":cnext<CR>")
+vim.keymap.set("n", "<leader>k", ":cprev<CR>")
 
 vim.keymap.set("n", "<leader>o", ":update<CR>:source<CR>")
 vim.keymap.set("n", "<leader>w", ":update<CR>")
 vim.keymap.set("n", "<leader>q", ":quit<CR>")
-vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
-
-vim.pack.add({
-    "https://github.com/neovim/nvim-lspconfig",
-    "https://github.com/mason-org/mason.nvim",
-    "https://github.com/mason-org/mason-lspconfig.nvim",
-    "https://github.com/nvim-lua/plenary.nvim",
-    "https://github.com/nvim-flutter/flutter-tools.nvim",
-    "https://github.com/nvim-treesitter/nvim-treesitter",
-    "https://github.com/ibhagwan/fzf-lua",
-    "https://github.com/windwp/nvim-autopairs",
-    "https://github.com/christoomey/vim-tmux-navigator",
-    "https://github.com/catppuccin/nvim",
-})
-
-require("catppuccin").setup({
-    flavour = "mocha",
-    no_italic = true,
-    no_bold = true,
-})
-vim.cmd.colorscheme("catppuccin")
-vim.cmd.highlight("Normal guibg=#101019")
-
-require("mason").setup({})
-require("mason-lspconfig").setup({
-    ensure_installed = { "lua_ls", "gopls", "rust_analyzer", "pyright",
-                         "clangd" },
-})
-
-vim.lsp.config("lua_ls", {
-    settings = {
-        Lua = {
-            workspace = {
-                library = vim.api.nvim_get_runtime_file("", true)
-            }
-        }
-    }
-})
-
-require("flutter-tools").setup({ fvm = true })
-
-require("nvim-treesitter.configs").setup({
-    ensure_installed = { "c", "lua", "markdown", "markdown_inline", "go",
-                         "rust", "python" },
-    sync_install = false,
-    auto_install = true,
-    modules = {},
-    ignore_install = {},
-    highlight = { enable = true },
-    indent = { enable = true },
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            node_incremental = "v",
-            scope_incremental = "<C-s>",
-            node_decremental = "V",
-        },
-    },
-})
-
-vim.keymap.set("n", "<C-h>", ":TmuxNavigateLeft<CR>")
-vim.keymap.set("n", "<C-j>", ":TmuxNavigateDown<CR>")
-vim.keymap.set("n", "<C-k>", ":TmuxNavigateUp<CR>")
-vim.keymap.set("n", "<C-l>", ":TmuxNavigateRight<CR>")
-vim.keymap.set("n", "<C-\\>", ":TmuxNavigatePrevious<CR>")
-
-vim.api.nvim_create_autocmd("LspAttach", {
-    callback = function()
-        vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
-        vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
-        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
-        vim.keymap.set("n", "gd", vim.lsp.buf.definition)
-    end,
-})
-
-vim.keymap.set("n", "<leader>sf", ":FzfLua files<CR>",
-               { desc = "Find Files" })
-vim.keymap.set("n", "<leader>sg", ":FzfLua live_grep<CR>",
-               { desc = "Live Grep" })
