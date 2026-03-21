3e75c5e18a59265c96f48dc127ae873c2e4a04e4
Ketan Sonar <ketan.sonar.798@gmail.com>
2026-03-21 15:12:30 +0530

    update nvim config

--- neovim-ide/lua/plugins/materialtheme.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "marko-cerovac/material.nvim",
-  enabled = true,
-  config = function()
-    vim.g.material_style = "deep ocean"
-    vim.cmd.colorscheme("material")
-  end
-}
---
+++ nvim/init.lua
@@ -0,0 +1,109 @@
+vim.g.mapleader = " "
+
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.wrap = false
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.signcolumn = "yes"
+vim.opt.winborder = "rounded"
+vim.opt.colorcolumn = "80"
+vim.opt.cursorline = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.swapfile = false
+vim.opt.undodir = os.getenv("HOME") .. "/.local/share/nvim/undodir"
+vim.opt.undofile = true
+vim.opt.scrolloff = 4
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.listchars = { tab = "» ", space = "·", nbsp = "␣" }
+vim.opt.path:append("**")
+vim.opt.wildignore:append("*/node_modules/*")
+vim.opt.wildignore:append("*/target/*")
+vim.opt.iskeyword:remove("_")
+
+vim.pack.add({
+    { src = "https://github.com/nvim-treesitter/nvim-treesitter",       version = "master" },
+    { src = "https://github.com/folke/tokyonight.nvim" },
+    { src = "https://github.com/neovim/nvim-lspconfig" },
+    { src = "https://github.com/mason-org/mason.nvim" },
+    { src = "https://github.com/mason-org/mason-lspconfig.nvim" },
+    { src = "https://github.com/folke/lazydev.nvim" },
+    { src = "https://github.com/rafamadriz/friendly-snippets" },
+    { src = "https://github.com/saghen/blink.cmp",                      version = vim.version.range("1.*") },
+    { src = "https://github.com/rachartier/tiny-inline-diagnostic.nvim" },
+    { src = "https://github.com/lewis6991/gitsigns.nvim" },
+    { src = "https://github.com/nvim-mini/mini.nvim" },
+    { src = "https://github.com/christoomey/vim-tmux-navigator" },
+    { src = "https://github.com/ibhagwan/fzf-lua" },
+})
+
+vim.cmd.colorscheme("tokyonight-night")
+
+require("nvim-treesitter.configs").setup({
+    auto_install = true,
+    highlight = { enable = true },
+    incremental_selection = {
+        enable = true,
+        keymaps = {
+            node_incremental = "v",
+            scope_incremental = "<C-s>",
+            node_decremental = "V",
+        },
+    },
+    indent = { enable = true },
+    modules = {},
+    sync_install = false,
+    ensure_installed = {},
+    ignore_install = {},
+})
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("lazydev").setup()
+
+require("blink.cmp").setup({
+    fuzzy = { implementation = "prefer_rust" },
+    keymap = { preset = "super-tab" },
+})
+
+require("tiny-inline-diagnostic").setup()
+require("gitsigns").setup()
+require("mini.icons").setup()
+require("mini.pairs").setup()
+require("mini.surround").setup()
+require("mini.statusline").setup()
+
+vim.keymap.set("n", "<leader>m", ":make<CR>")
+vim.keymap.set("n", "<leader>j", ":cnext<CR>")
+vim.keymap.set("n", "<leader>k", ":cprev<CR>")
+
+vim.keymap.set("n", "<leader>o", ":update<CR>:source<CR>")
+vim.keymap.set("n", "<leader>w", ":update<CR>")
+vim.keymap.set("n", "<leader>q", ":quit<CR>")
+
+vim.keymap.set("v", "<C-y>", '"+y')
+vim.keymap.set("n", "<C-p>", '"+p')
+vim.keymap.set("v", "<C-p>", '"+P')
+
+vim.keymap.set("n", "<leader>lf", vim.lsp.buf.format)
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float)
+vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+vim.keymap.set("n", "gd", vim.lsp.buf.definition)
+
+vim.keymap.set("n", "<leader>sf", ":FzfLua files<CR>")
+vim.keymap.set("n", "<leader>sg", ":FzfLua live_grep<CR>")
+vim.keymap.set("n", "<leader>sb", ":FzfLua buffers<CR>")
+vim.keymap.set("n", "<leader>sm", ":FzfLua manpages<CR>")
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+    desc = "Highlight when yanking (copying) text",
+    group = vim.api.nvim_create_augroup("highlight-yank", { clear = true }),
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+})
+vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
