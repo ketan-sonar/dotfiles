efe061a648a7831b19f6e87fcde0bfa57b92db8c
Ketan Sonar <ketan.sonar.798@gmail.com>
2026-05-22 00:11:51 +0530

    update nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -43,15 +43,103 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-    { "folke/tokyonight.nvim" },
+    {
+        "catppuccin/nvim",
+        name = "catppuccin",
+        lazy = false,
+        priority = 1000,
+        config = function()
+            require("catppuccin").setup({
+                transparent_background = true,
+                integrations = { nvimtree = true },
+            })
+            vim.cmd.colorscheme("catppuccin-mocha")
+        end,
+    },
+
     { "m00qek/baleia.nvim" },
     { "rachartier/tiny-inline-diagnostic.nvim" },
     { "nvim-mini/mini.nvim" },
 
     {
+        "nvim-lualine/lualine.nvim",
+        dependencies = { "nvim-mini/mini.nvim" },
+        config = function()
+            require("lualine").setup({
+                options = {
+                    globalstatus = true,
+                    component_separators = "",
+                    section_separators = { left = "", right = "" },
+                },
+                sections = {
+                    lualine_a = {
+                        { "mode", separator = { left = "", right = "" }, right_padding = 2 },
+                    },
+                    lualine_b = {
+                        { "branch", icon = "" },
+                        "diagnostics",
+                    },
+                    lualine_c = { "filename" },
+                    lualine_x = { "filetype" },
+                    lualine_y = { "progress" },
+                    lualine_z = {
+                        { "location", separator = { left = "", right = "" }, left_padding = 2 },
+                    },
+                },
+            })
+        end,
+    },
+
+    {
         "nvim-treesitter/nvim-treesitter",
-        branch = "master",
-        build = ":TSUpdate",
+        branch = "main",
+        config = function()
+            require("nvim-treesitter").setup()
+
+            ---@param buf integer
+            ---@param language string
+            local function treesitter_try_attach(buf, language)
+                -- Check if a parser exists and load it
+                if not vim.treesitter.language.add(language) then return end
+                -- Enable syntax highlighting and other treesitter features
+                vim.treesitter.start(buf, language)
+
+                -- Enable treesitter based folds
+                -- For more info on folds see `:help folds`
+                -- vim.wo.foldexpr = 'v:lua.vim.treesitter.foldexpr()'
+                -- vim.wo.foldmethod = 'expr'
+
+                -- Check if treesitter indentation is available for this language, and if so enable it
+                -- in case there is no indent query, the indentexpr will fallback to the vim's built in one
+                local has_indent_query = vim.treesitter.query.get(language, 'indents') ~= nil
+
+                -- Enable treesitter based indentation
+                if has_indent_query then vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()" end
+            end
+
+            local available_parsers = require('nvim-treesitter').get_available()
+            vim.api.nvim_create_autocmd('FileType', {
+                callback = function(args)
+                    local buf, filetype = args.buf, args.match
+
+                    local language = vim.treesitter.language.get_lang(filetype)
+                    if not language then return end
+
+                    local installed_parsers = require('nvim-treesitter').get_installed 'parsers'
+
+                    if vim.tbl_contains(installed_parsers, language) then
+                        -- Enable the parser if it is already installed
+                        treesitter_try_attach(buf, language)
+                    elseif vim.tbl_contains(available_parsers, language) then
+                        -- If a parser is available in `nvim-treesitter`, auto-install it and enable it after the installation is done
+                        require('nvim-treesitter').install(language):await(function() treesitter_try_attach(buf, language) end)
+                    else
+                        -- Try to enable treesitter features in case the parser exists but is not available from `nvim-treesitter`
+                        treesitter_try_attach(buf, language)
+                    end
+                end,
+            })
+        end
     },
 
     {
@@ -95,27 +183,6 @@ require("lazy").setup({
     },
 })
 
-vim.cmd.colorscheme("tokyonight-night")
-vim.cmd.highlight("Normal guibg=NONE ctermbg=NONE")
-
-require("nvim-treesitter.configs").setup({
-    auto_install = true,
-    highlight = { enable = true },
-    incremental_selection = {
-        enable = true,
-        keymaps = {
-            node_incremental = "v",
-            scope_incremental = "<C-s>",
-            node_decremental = "V",
-        },
-    },
-    indent = { enable = true },
-    modules = {},
-    sync_install = false,
-    ensure_installed = {},
-    ignore_install = {},
-})
-
 require("mason").setup()
 require("mason-lspconfig").setup()
 require("lazydev").setup()
@@ -141,7 +208,6 @@ require("mini.icons").setup()
 require("mini.icons").mock_nvim_web_devicons()
 require("mini.pairs").setup()
 require("mini.surround").setup()
-require("mini.statusline").setup()
 
 require("nvim-tree").setup({
     view = { width = 40, side = "right", adaptive_size = true },
